e6df807741bdd25877dca6220850030b1b14b88c
stephen.h.day <stephen.h.day@a60eed84-fc3d-0410-a09c-0d1a7a6707c9>
2008-12-04 02:32:31 +0000

    work on documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,6 +25,8 @@ sys.path.append(os.path.abspath('../../'))
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest']
 
+doctest_test_doctest_blocks = ''
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
