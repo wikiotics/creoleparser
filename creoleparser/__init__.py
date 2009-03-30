47aff9af0f8b83a53769eb428bb1843a33180c69
stephen.h.day <stephen.h.day@a60eed84-fc3d-0410-a09c-0d1a7a6707c9>
2009-03-30 03:25:22 +0000

    refactoring of argument parsing work on docs

--- creoleparser/__init__.py
+++ creoleparser/__init__.py
@@ -5,14 +5,16 @@
 # This module is part of Creoleparser and is released under
 # the MIT License: http://www.opensource.org/licenses/mit-license.php
 #
-from string import lower
+import string
+import keyword
+
 from core import Parser, ArgParser
 from dialects import (creole11_base, creole10_base, creepy_base,
                     create_dialect, Creole10)
 
 __docformat__ = 'restructuredtext en'
 
-__version__ = '0.6'
+__version__ = '0.6.1'
 
 creole2html = Parser(dialect=create_dialect(creole10_base), method='html')
 """This is a pure Creole 1.0 parser created for convenience"""
@@ -20,7 +22,10 @@ creole2html = Parser(dialect=create_dialect(creole10_base), method='html')
 text2html = Parser(dialect=create_dialect(creole11_base), method='html')
 """This is a Creole 1.0 parser (+ additions) created for convenience"""
 
-parse_args = ArgParser(dialect=creepy_base(key_func=lower), force_strings=True)
+parse_args = ArgParser(dialect=creepy_base(),key_func=string.lower,
+                       illegal_keys=keyword.kwlist + ['macro_name',
+                         'arg_string', 'body', 'isblock', 'environ', 'macro'],
+                       force_strings=True)
 """Function for parsing macro arg_strings using a relaxed xml style"""
 
 def _test():
